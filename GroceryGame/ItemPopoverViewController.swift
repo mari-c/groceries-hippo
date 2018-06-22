b539c33068fdd71398c486596154cbc6ded954bf
mariana <384+c1t0b@users.noreply.github.ugrad.cs.ubc.ca>
2018-06-22 23:11:19 +0200

    Default item quantity to add is 1

--- GroceryGame/ItemPopoverViewController.swift
+++ GroceryGame/ItemPopoverViewController.swift
@@ -30,6 +30,8 @@ class ItemPopoverViewController: UIViewController, UITextFieldDelegate, UIPopove
         nameTextField.tag = NAME_TF_TAG
         quantityTextField.delegate = self
         quantityTextField.tag = QUANTITY_TF_TAG
+        
+        quantityStepper.value = Double(quantityTextField.text!)!
     }
 
     /*
@@ -47,11 +49,6 @@ class ItemPopoverViewController: UIViewController, UITextFieldDelegate, UIPopove
         return true
     }
     
-    func textFieldDidBeginEditing(_ textField: UITextField) {
-        // Disable Ok button whilst editing
-        // okButton.isEnabled = false
-    }
-    
     func textFieldDidEndEditing(_ textField: UITextField) {
         if textField.tag == QUANTITY_TF_TAG {
             quantityStepper.value = Double(quantityTextField.text!)!
@@ -64,10 +61,8 @@ class ItemPopoverViewController: UIViewController, UITextFieldDelegate, UIPopove
         return .none
     }
     
-    
     // MARK: Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         super.prepare(for: segue, sender: sender)
         
